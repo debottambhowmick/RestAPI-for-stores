a8b3249c1c435a6c13b258b30dd74b71b54daaa7
Debottam Bhowmick <debottambhowmick@gmail.com>
2024-10-23 02:49:08 +0530

    CURD api added for items

--- app.py
+++ app.py
@@ -48,9 +48,9 @@ def create_item():
     return item, 201 
 
 #get all items
-app.get("/item")
+@app.get("/item")
 def get_all_items():
-    return {items:list(items.values())}
+    return {"items":list(items.values())}
 
 # getting an individual store           
 @app.get("/store/<string:store_id>")
@@ -60,10 +60,33 @@ def  get_store(store_id):
         except KeyError:
             abort(404, message="Store not found!!" )
 
-# get item of a store
+# get item
 @app.get("/item/<string:item_id>")
 def get_item(item_id):
     try:
         return items[item_id]
     except KeyError:
         abort(404, message="Item not found!!" )
+
+# Delete an item
+@app.delete("/item/<string:item_id>")
+def delete_item(item_id):
+    try:
+        del items[item_id]
+        return {"message": "Item deleted."}
+    except KeyError:
+        abort(404, message="Item not found!!" )
+
+@app.put("/item/<string:item_id>")
+def update_item(item_id):
+    item_data = request.get_json()
+    if "price" not in item_data or "name" not in item_data:
+        abort(400, message="Bad request. Ensure 'price' and 'name' are included in the JSON payload.")
+    try:
+        item = items[item_id]
+        item["name"] = item_data["name"]
+        item["price"] = item_data["price"]
+        ## item |= item_data  # short cut to update dictionary
+        return item
+    except KeyError:
+        abort(404, "Item not Found.")

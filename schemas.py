1604147706aaa7374b9bfe511f662b64366c2390
Debottam Bhowmick <debottambhowmick@gmail.com>
2024-10-24 21:55:37 +0530

    marshmallow schema is also been updated for a common table to support many-to-many relationship

--- schemas.py
+++ schemas.py
@@ -20,7 +20,8 @@ class ItemUpdateSchema(Schema):
 
 class ItemSchema(PlainItemSchema):
     store_id = fields.Int(required=True, load_only=True)
-    store = fields.Nested(PlainStoreSchema(), dump_only=True) 
+    store = fields.Nested(PlainStoreSchema(), dump_only=True)  
+    tags = fields.List(fields.Nested(PlainTagSchema(), dump_only=True))
  
 class StoreSchema(PlainStoreSchema):
     items = fields.List(fields.Nested(PlainItemSchema()), dump_only=True)
@@ -29,3 +30,11 @@ class StoreSchema(PlainStoreSchema):
 class TagSchema(PlainTagSchema):
     store_id = fields.Int(load_only=True)# Removed "required-"
     store = fields.Nested(PlainStoreSchema(), dump_only=True) 
+    items = fields.List(fields.Nested(PlainItemSchema(), dump_only=True))
+
+
+# for returning related items and tags we need this
+class TagAndItemSchema(Schema):
+    message = fields.str()
+    item = fields.Nested(ItemSchema)
+    tag = fields.Nested(TagSchema)
